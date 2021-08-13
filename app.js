9ea40a7a561cedcb7c1fc9e8046fa576c4f34b3e
wblanck <blanck.william@gmail.com>
2021-08-13 19:13:02 +0200

    finished html/css for slider

---
+++ app.js
@@ -0,0 +1,7 @@
+let nextImg = document.querySelector(".slideshow__btn-right");
+let slideShowList = document.querySelectorAll(".slideshow__item");
+
+
+nextImg.addEventListener("click", () => {
+  console.log("hey");
+});
